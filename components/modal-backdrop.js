5632a4ffedd020c2872afdf2491cd3ab86470a26
George Karountzos <george3-2001@hotmail.com>
2024-08-13 21:54:10 +0300

    changes

--- components/modal-backdrop.js
+++ components/modal-backdrop.js
@@ -3,4 +3,6 @@ import { useRouter } from "next/navigation";
 
 export default function ModalBackdrop() {
   const router = useRouter();
+
+  return <div className="modal-backdrop" onClick={router.back} />
 }
